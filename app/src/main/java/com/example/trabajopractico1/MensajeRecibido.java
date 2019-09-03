45d6793bd13ff4dd1a145e95d3bb7dddfa1f623b
rb2711 <rodobertero@gmail.com>
2019-09-02 21:54:32 -0300

    modificacion Trabajo Practico 1

--- app/src/main/java/com/example/trabajopractico1/MensajeRecibido.java
+++ app/src/main/java/com/example/trabajopractico1/MensajeRecibido.java
@@ -7,6 +7,7 @@ import android.widget.Toast;
 
 public class MensajeRecibido extends BroadcastReceiver {
     @Override
+    int a;
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context,"Ha recibido un mensaje nuevo",Toast.LENGTH_LONG).show();
     }
